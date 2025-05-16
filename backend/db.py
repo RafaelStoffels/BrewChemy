a1b6c62d686c34c4ef1c048fd5fc14e07099757e
Rafael Stoffels <stoffels.rafael@gmail.com>
2025-05-16 15:45:34 -0300

    Environments Variables Improvments
    Environments Variables Improvments

--- backend/db.py
+++ backend/db.py
@@ -9,16 +9,10 @@ db = SQLAlchemy()
 
 def configure_db(app):
 
-    if os.getenv('ENVIRONMENT') == 'production':
-        db_uri = (
-            f"postgresql://{os.getenv('DB_USER_PROD')}:{os.getenv('DB_PASSWORD_PROD')}"
-            f"@{os.getenv('DB_HOST_PROD')}:{os.getenv('DB_PORT_PROD')}/{os.getenv('DB_NAME_PROD')}"
-        )
-    else:
-        db_uri = (
-            f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-            f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-        )
+    db_uri = (
+        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )    
 
     # Database configuration in Flask
     app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
