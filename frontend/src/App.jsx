a8c114eaa7306f0e8f584febf379330d922b0db8
Rafael Stoffels <stoffels.rafael@gmail.com>
2025-08-22 15:28:35 -0300

    refactor: renaming CRA old_frontend and creating Vite project
    refactor: renaming CRA old_frontend and creating Vite project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,90 +1,35 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { AuthProvider } from './context/AuthContext';
-import { SidebarProvider } from './context/SidebarContext';
-
-import Changelog from './Pages/Changelog/Form';
-import ChangePassword from './Pages/Logon/ChangePassword';
-import CreateAccount from './Pages/Logon/CreateAccount';
-import EmailSent from './Pages/Logon/CreateAccount/EmailSent';
-import ForgotPassword from './Pages/Logon/ForgotPassword/Form';
-import Logon from './Pages/Logon/Form';
-import Main from './Pages/Main';
-import EquipmentList from './Pages/Equipments/List';
-import Equipment from './Pages/Equipments/Form';
-import FermentableList from './Pages/Inventories/Fermentables/List';
-import Fermentable from './Pages/Inventories/Fermentables/Form';
-import HopList from './Pages/Inventories/Hops/List';
-import Hop from './Pages/Inventories/Hops/Form';
-import MiscList from './Pages/Inventories/Miscs/List';
-import Misc from './Pages/Inventories/Miscs/Form';
-import RecipeList from './Pages/Recipes/List';
-import Recipe from './Pages/Recipes/Form';
-import YeastList from './Pages/Inventories/Yeasts/List';
-import Yeast from './Pages/Inventories/Yeasts/Form';
-
-import Layout from './Components/Layout';
-import SettingsForm from './Pages/Settings/Form';
-
-import 'react-toastify/dist/ReactToastify.css';
-import './Styles/global.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  useEffect(() => {
-    document.title = 'Brewchemy';
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <AuthProvider>
-      <SidebarProvider>
-        <Router>
-          <ToastContainer autoClose={3000} position="top-right" />
-          <Routes>
-
-            {/* no need layout */}
-            <Route path="/" element={<Logon />} />
-            <Route path="/CreateAccount" element={<CreateAccount />} />
-            <Route path="/EmailSent" element={<EmailSent />} />
-            <Route path="/ForgotPassword" element={<ForgotPassword />} />
-            <Route path="/ChangePassword" element={<ChangePassword />} />
-
-            {/* layout - sidebar */}
-            <Route element={<Layout />}>
-              <Route path="/Changelog" element={<Changelog />} />
-              <Route path="/Main" element={<Main />} />
-              <Route path="/EquipmentList" element={<EquipmentList />} />
-              <Route path="/Equipments/:recordUserId/:id/details" element={<Equipment />} />
-              <Route path="/Equipments/:recordUserId/:id/edit" element={<Equipment />} />
-              <Route path="/Equipments/new" element={<Equipment />} />
-              <Route path="/FermentableList" element={<FermentableList />} />
-              <Route path="/Fermentables/:recordUserId/:id/details" element={<Fermentable />} />
-              <Route path="/Fermentables/:recordUserId/:id/edit" element={<Fermentable />} />
-              <Route path="/Fermentables/new" element={<Fermentable />} />
-              <Route path="/HopList" element={<HopList />} />
-              <Route path="/Hops/:recordUserId/:id/details" element={<Hop />} />
-              <Route path="/Hops/:recordUserId/:id/edit" element={<Hop />} />
-              <Route path="/Hops/new" element={<Hop />} />
-              <Route path="/MiscList" element={<MiscList />} />
-              <Route path="/Miscs/:recordUserId/:id/details" element={<Misc />} />
-              <Route path="/Miscs/:recordUserId/:id/edit" element={<Misc />} />
-              <Route path="/Miscs/new" element={<Misc />} />
-              <Route path="/RecipeList" element={<RecipeList />} />
-              <Route path="/Recipes/:id/details" element={<Recipe />} />
-              <Route path="/Recipes/:id/edit" element={<Recipe />} />
-              <Route path="/Recipes/new" element={<Recipe />} />
-              <Route path="/YeastList" element={<YeastList />} />
-              <Route path="/Yeasts/:recordUserId/:id/details" element={<Yeast />} />
-              <Route path="/Yeasts/:recordUserId/:id/edit" element={<Yeast />} />
-              <Route path="/Yeasts/new" element={<Yeast />} />
-              <Route path="/Settings" element={<SettingsForm />} />
-            </Route>
-
-          </Routes>
-        </Router>
-      </SidebarProvider>
-    </AuthProvider>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
